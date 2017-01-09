8c7e4c5f63a80229b3f8a13efc99f55b580b8da9
tomvanboghout <tvboghout@gmail.com>
2017-01-09 13:56:30 +0100

    Finisched module 2 -

--- main.ts
+++ main.ts
@@ -1,27 +1,58 @@
-import {Observable} from 'rxjs/Observable';
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/filter";
+import {Observable} from 'rxjs';
 
-let numbers = [1,5,10];
-//let source = Observable.from(numbers);
-let source = Observable.create(observer => {
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-        if (index < numbers.length) {
-            setTimeout(produceValue, 200);
-        } else {
-            observer.complete();
-        }
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText)
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    }).retryWhen(retryStrategy());
+    
+}
+
+function loadWithFetch(url: string) {
+    return Observable.fromPromise(fetch(url).then(r => r.json()));
+}
+
+function retryStrategy() {
+    return function(errors) {
+        return errors
+                .scan((acc, value) => {
+                    console.log(acc, value);
+                    return acc + 1
+                }, 0 )
+                .takeWhile(acc => acc < 4)
+                .delay(1000);
     }
+}
 
-    produceValue();
-}).map(n => n*2)
-.filter(n => n > 4);
+function renderMovies(movies) {
+    movies.forEach(m => {
+                let div = document.createElement("div");
+                div.innerText = m.title;
+                output.appendChild(div);
+            });
+}
 
-source.subscribe(
-    value => console.log(`Avalue: ${value}`),
-    e => console.log(`error: ${e}`),
-    () => console.log("complete")
-);
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+    );
+
+
